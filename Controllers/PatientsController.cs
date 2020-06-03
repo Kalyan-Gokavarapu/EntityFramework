660826ef98398d0d879bd16e524e9f6b84a707d8
Kalyan Gokavarapu <gokavarapu.kalyan@hotmail.com>
2020-06-04 00:21:21 +0530

    Added Patient controller

---
+++ Controllers/PatientsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EntityFramework.Models;
+
+namespace EntityFramework.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientsController : ControllerBase
+    {
+        private readonly PatientContext _context;
+
+        public PatientsController(PatientContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Patients
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Patient>>> GetPatient()
+        {
+            return await _context.Patient.ToListAsync();
+        }
+
+        // GET: api/Patients/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Patient>> GetPatient(int id)
+        {
+            var patient = await _context.Patient.FindAsync(id);
+
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            return patient;
+        }
+
+    }
+}
--- Models/PatientContext.cs
+++ Models/PatientContext.cs
@@ -21,18 +21,118 @@ namespace EntityFramework.Models
             //modelBuilder.Entity<Patient>().HasData(
             //    new Patient
             //    {
-            //        id =1,
-            //        MRN = "12345",
-            //        Name = "Patient 1"
+            //        id = 1,
+            //        MRN = "11111",
+            //        Name = "Patient 1",
+            //        Exams = new List<Exam>()
+            //        {
+            //            new Exam{
+            //            ExamCode ="cd1",
+            //            Modality ="mod1"
+            //            },
+            //            new Exam{
+            //            ExamCode ="cd1",
+            //            Modality ="mod1"
+            //            },
+            //            new Exam{
+            //            ExamCode ="cd1",
+            //            Modality ="mod1"
+            //            }
+            //        },
+            //        Addresses = new List<Address>()
+            //        {
+            //            new Address{
+            //            Country="India",
+            //            State="Karnataka",
+            //            Pin="100200"
+            //            }
+            //        },
+            //        Emails = new List<Email>()
+            //        {
+            //            new Email{
+            //            EmailAddress="mail@web.com"
+            //            },
+            //            new Email{
+            //            EmailAddress="mali@ai.com"
+            //            }
+            //        }
             //    },
-            //    new Patient { 
-                
+            //    new Patient
+            //    {
+            //        id = 2,
+            //        MRN = "22222",
+            //        Name = "Patient 2",
+            //        Exams = new List<Exam>()
+            //        {
+            //            new Exam{
+            //            ExamCode ="cd1",
+            //            Modality ="mod1"
+            //            },
+            //            new Exam{
+            //            ExamCode ="cd1",
+            //            Modality ="mod1"
+            //            },
+            //            new Exam{
+            //            ExamCode ="cd1",
+            //            Modality ="mod1"
+            //            }
+            //        },
+            //        Addresses = new List<Address>()
+            //        {
+            //            new Address{
+            //            Country="India",
+            //            State="Karnataka",
+            //            Pin="100200"
+            //            }
+            //        },
+            //        Emails = new List<Email>()
+            //        {
+            //            new Email{
+            //            EmailAddress="mail@web.com"
+            //            },
+            //            new Email{
+            //            EmailAddress="mali@ai.com"
+            //            }
+            //        }
             //    },
-            //    new Patient { 
-                
-            //    }
-            //    );
-            
+            //    new Patient
+            //    {
+            //        id = 3,
+            //        MRN = "33333",
+            //        Name = "Patient 3",
+            //        Exams = new List<Exam>()
+            //        {
+            //            new Exam{
+            //            ExamCode ="cd1",
+            //            Modality ="mod1"
+            //            },
+            //            new Exam{
+            //            ExamCode ="cd1",
+            //            Modality ="mod1"
+            //            },
+            //            new Exam{
+            //            ExamCode ="cd1",
+            //            Modality ="mod1"
+            //            }
+            //        },
+            //        Addresses = new List<Address>()
+            //        {
+            //            new Address{
+            //            Country="India",
+            //            State="Karnataka",
+            //            Pin="100200"
+            //            }
+            //        },
+            //        Emails = new List<Email>()
+            //        {
+            //            new Email{
+            //            EmailAddress="mail@web.com"
+            //            },
+            //            new Email{
+            //            EmailAddress="mali@ai.com"
+            //            }
+            //        }
+            //    });
         }
     }
 }
